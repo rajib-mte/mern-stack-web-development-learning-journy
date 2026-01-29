764841766ddfcf477ed8e2e5c5da2d3f3f465c1f
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-01-29 15:21:02 +0600

    added: 13.4 module

---
+++ 1.programming-hero/Module-13.4_what-is-a-variable-five-things-you-need-to-declare-a-variable/README.md
@@ -0,0 +1,1 @@
+cd /e/mte-journy/learning-full-stack-web-development-with-mern/1.programming-hero/Module-13.2_ntroduction-to-javascript-run-javascript-in-vscode
---
+++ 1.programming-hero/Module-13.4_what-is-a-variable-five-things-you-need-to-declare-a-variable/first.js
@@ -0,0 +1,21 @@
+/*
+1. A variable must start with a letter, underscore (_), or dollar sign ($).
+2. A variable name cannot start with a number.
+3. A variable name can only contain letters, numbers, underscores (_), or dollar signs ($).
+4. Variable names are case-sensitive (e.g., myVar and myvar are different variables).
+5. Reserved words (like JavaScript keywords) cannot be used as variable names.
+* Varible ==> varyu+b
+* 5 thinngs to declare a variable
+*/
+
+var weight = 65; // valid
+var price = 38;
+var year = 2023;
+var money = 2005;
+
+var age;
+console.log(weight);
+console.log("22 Hello, Programming Hero!");
+console.log(price);
+console.log(year);
+console.log(money);
