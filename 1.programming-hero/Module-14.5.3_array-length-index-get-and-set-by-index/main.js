1165d0a2e8c1d4f643cdde31c115eb4896d6073f
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-02-20 11:50:08 +0600

    added: 14.5.3 module

---
+++ 1.programming-hero/Module-14.5.3_array-length-index-get-and-set-by-index/main.js
@@ -0,0 +1,15 @@
+const Numbers = [1, 2, 3, 4, 5];
+console.log(Numbers.length);
+console.log(Numbers);
+
+// get element value by index
+console.log(Numbers[0]);
+const fourth = Numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+
+Numbers[1] = 999;
+console.log(Numbers);
