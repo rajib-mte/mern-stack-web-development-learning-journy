a557003eeaba9f9e43d4d9c2d3f6a4815876c043
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-02-20 17:32:12 +0600

    added: 15.1 module

---
+++ 1.programming-hero/Module-15.1_what-is-a-loop-explore-for-loop/main.js
@@ -0,0 +1,12 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+console.log(numbers);
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["orange", "apple", "banna", "jackfruit", "watermelon"];
+
+for (const x of fruits) {
+  console.log(x);
+}
