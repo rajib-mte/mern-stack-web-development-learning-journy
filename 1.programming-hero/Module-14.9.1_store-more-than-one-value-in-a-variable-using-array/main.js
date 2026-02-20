15c27b18afec51e50944a2788306359185c0e0e7
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-02-20 11:42:11 +0600

    added: 14.5.1 module

---
+++ 1.programming-hero/Module-14.9.1_store-more-than-one-value-in-a-variable-using-array/main.js
@@ -0,0 +1,7 @@
+const num1 = 12;
+const num2 = 22;
+
+const numbers = [12, 22, 54, 65];
+const friends = ["Abul", "babul", "cabul", "dabul", "bulbul"];
+const passed = [true, false, true, false];
+const mixedArray = [12, "alahbad", true, 45.566];
